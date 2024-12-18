78921e512825b3a6f70f6b3b5285bdeaa1576f75
Madhav Girish Nair <madhavgirish02@gmail.com>
2024-12-18 17:43:53 +0100

    feat:gemini summarization added

--- rag_chatbot/pdf_chatbot.py
+++ rag_chatbot/pdf_chatbot.py
@@ -1,47 +1,52 @@
 import os
+import logging
+from dotenv import load_dotenv
 
-from langchain_community.document_loaders import PyMuPDFLoader
-from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_community.vectorstores import Pinecone
-from langchain_community.llms import HuggingFaceHub
-import pinecone
-from pinecone import ServerlessSpec
+from google import genai
+
+from langchain.prompts import PromptTemplate
+from langchain_core.messages import AIMessage
+from langchain_core.runnables import RunnableLambda
+from langchain_community.document_loaders import PyPDFLoader
+
+load_dotenv()
+logging.basicConfig(level=logging.INFO)
 
 
 class PDFChatbot:
     def __init__(self, pdf_path):
         self.pdf_path = pdf_path
+        self.model_name = 'gemini-2.0-flash-exp'
 
-    def __parse_pdf(self):
-        loader = PyMuPDFLoader(self.pdf_path)
-        docs = loader.load()
-        return docs
-
-    def __initialize_vector_space(self, docs):
-        pc = pinecone.Pinecone(api_key=os.getenv('PINECONE_API_KEY'))
-        index_name = 'pdf_chatbot'
-        embeddings = HuggingFaceEmbeddings()
-
-        if index_name not in pc.list_indexes():
-            pc.create_index(name=index_name, metric='cosine', dimension=768, spec=ServerlessSpec(cloud='gcp', region='europe-west4'))
-            docsearch = Pinecone.from_documents(docs, embeddings, index_name=index_name)
-        else:
-            docsearch = Pinecone.from_existing_index(index_name, embeddings)
-
-    def __initialize_llm(self):
-        repo_id = "mistralai/Mixtral-8x7B-Instruct-v0.1"
-        llm = HuggingFaceHub(
-            repo_id=repo_id,
-            model_kwargs={"temperature": 0.8, "top_k": 50},
-            huggingfacehub_api_token=os.getenv('HUGGINGFACE_API_KEY')
+        self.client = genai.Client(
+            api_key=os.getenv("GEMINI_API_KEY"),
+            http_options={"api_version": "v1alpha"},
         )
-        return llm
+
+    def load_pdf(self):
+        loader = PyPDFLoader(self.pdf_path)
+        docs = loader.load()
+        texts = [doc.page_content for doc in docs]
+        return texts
+
+    def summarize_texts(self, texts):
+        prompt = """You are an expert tasked with summarizing text for retrieval. \
+                    These summaries will be embedded and used to retrieve the raw text. \
+                    Give a concise summary of the text that is well optimized for retrieval. Text: {element} """
+        summaries = []
+        for text in texts:
+            response = self.client.models.generate_content(
+                model=self.model_name, contents=[text, prompt]
+            )
+            summaries.extend(response.text)
+
+        with open('summaries.txt', 'w') as f:
+            f.write('\n===\n'.join(summaries))
 
 
-    def chat(self):
-        documents = self.__parse_pdf()
+if __name__ == "__main__":
+    pdf_path = "./pdfs/quaternions.pdf"
+    chatbot = PDFChatbot(pdf_path)
+    texts = chatbot.load_pdf()
+    chatbot.summarize_texts(texts)
 
-
-if __name__ == '__main__':
-    chatbot = PDFChatbot('./pdfs/quaternions.pdf')
-    chatbot.chat()
